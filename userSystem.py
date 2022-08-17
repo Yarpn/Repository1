2d2f24f6e1b03c95a6317bf87eca0e9269df34c3
Yariv <yariv@Yarivs-MacBook-Pro.local>
2022-08-17 16:12:45 +0300

    bla bla

---
+++ userSystem.py
@@ -0,0 +1,21 @@
+
+from string import ascii_lowercase, ascii_uppercase, digits
+from random import choice
+import random
+# print('\n'.join([''.join([random.choice(ascii_lowercase + ascii_uppercase + digits) for i in range(10)]) for i in range(10)]))
+# print(digits,'\n')
+# print(ascii_lowercase,'\n')
+# print(ascii_uppercase)
+
+print('\n'.join([''.join([random.choice(ascii_lowercase + ascii_uppercase + digits) for i in range(10)]) for i in range(10)]))
+# random_passw
+# name = input('Please enter your name: ')
+# password = input('Please enter your password: ')
+
+
+
+
+
+
+
+
